9611c0d69d76f7335534bc65bc78eaf933d23f9a
Rolf <rolf@rolf-laptop.(none)>
2011-08-14 23:08:10 +0200

    Added setup.py

---
+++ Physics/setup.py
@@ -0,0 +1,13 @@
+# setup.py
+
+from distutils.core import setup, Extension
+
+setup(name="physics",
+      py_modules=['physics'], 
+      ext_modules=[Extension("_physics",
+                     ["physics.i","PhysicsCore.cpp","Objects/PhysicsObject.cpp","Objects/Constraint.cpp","CollisionDetection/SeperatingAxis.cpp","Shapes/Polygon.cpp","Math/Math.cpp"],
+                     swig_opts=['-c++']
+                  )]
+      
+)
+
